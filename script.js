eaafec7a4c601a9f66ecd8d96237c2b5e22062d8
Alexandr Uvarovskii <motorstein@mail.ru>
2023-01-24 01:35:45 +0300

    JS new

---
+++ script.js
@@ -0,0 +1,49 @@
+let a = 20;
+alert(a);
+
+let iPhoneCreate = 2007;
+alert(iPhoneCreate);
+
+let JavaScriptCreate = "Brendan Eich";
+alert(JavaScriptCreate);
+
+let variable10 = 10;
+let variable2 = 2;
+let addition = variable10 + variable2;
+alert(addition);
+let subtraction = variable10 - variable2;
+alert(subtraction);
+let multiplication = variable10 * variable2;
+alert(multiplication);
+let division = variable10 / variable2;
+alert(division);
+
+let result = variable2 ** 5;
+alert(result);
+
+let variable9 = 9;
+let module = variable9 % variable2;
+alert(module);
+
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num = ++num;
+num = --num;
+alert(num);
+
+let age = prompt("Сколько вам лет?");
+alert(age);
+
+let user = {
+  name: "Alex",
+  friends: 34,
+};
+let info = prompt("Какую информацию хотите узнать о пользователе?");
+alert(info);
+
+let guest = prompt("Как вас зовут?");
+let hello = guest;
+alert(`Привет, ${guest}`);
